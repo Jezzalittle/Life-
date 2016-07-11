36481f30b1c174398598a97edd43932fce96d203
Jezzalittle <jezzalittle20@gmail.com>
2016-07-11 20:25:22 +1000

    Random Food Spawner

---
+++ Life/Assets/Scripts/Food.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Food : MonoBehaviour {
+
+
+    public GameObject prefab;
+    float timeLeft = 1.0f;
+
+    void Update () {
+        timeLeft = timeLeft - Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            Instantiate(prefab, new Vector3(Random.Range(5.0f, -5.0f), 1, Random.Range(5.0f, -5.0f)), Quaternion.identity);
+            timeLeft = 1.0f;
+        }
+    }
+}
