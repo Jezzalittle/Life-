647a2ff8c330500d667fd14e5c8b451e8dd77517
Jezzalittle <jezzalittle20@gmail.com>
2016-07-19 13:39:56 +1000

    ai changes

--- Life/Assets/Scripts/Tringle_work.cs
+++ Life/Assets/Scripts/Tringle_work.cs
@@ -6,10 +6,15 @@ public class Tringle_work : MonoBehaviour {
     public int[] aiMovement;
     public GameObject spices;
     public float startEatTime = 5.0f;
-
+    private int Desition = 5;
+    private bool foodFound;
+    private Quaternion rotNow, rotSoon;
+    private Vector3 posNow, posSoon;
+    GameObject closest = null;
+    
     public int decisionMaking(int eat, int move, int brawl, int reproduce, int dontMove)
     {
-       int tempRand = Random.Range(0, 99);
+       int tempRand = Random.Range(1, 100);
        if(tempRand < eat)
         {
             return 0;
@@ -18,56 +23,109 @@ public class Tringle_work : MonoBehaviour {
         {
             return 1;
         }
-        else if (tempRand < eat + move + brawl && tempRand > move)
+        else if (tempRand < eat + move + brawl && tempRand > move + eat)
         {
             return 2;
         }
-        else if (tempRand < eat + move + brawl + reproduce && tempRand > brawl)
+        else if (tempRand < eat + move + brawl + reproduce && tempRand > brawl + eat + move)
         {
             return 3;
         }
-        else if (tempRand < eat + move + brawl + reproduce + dontMove && tempRand > reproduce)
+        else if (tempRand < eat + move + brawl + reproduce + dontMove && tempRand > reproduce + eat + move + brawl)
         {
             return 4;
         }
         return 9;
     }
-    GameObject closest = null;
+
+    void Start()
+    {
+        posNow = transform.position;
+        posSoon = transform.position;
+        rotNow = transform.rotation;
+        rotSoon = transform.rotation;
+    }
+
+
     void Update()
     {
-        //nearest food
-        GameObject []
-        gos;
+ 
+        if (Desition == 5)
+        {
+            Desition = decisionMaking(20, 20, 20, 20, 20);
+        }
+        if(Desition == 0)
+        {
+            //Eat
+            if(foodFound == false)
+            {
+                findFood();
+                foodFound = true;
+            }
+        }
+        if (Desition == 1)
+        {
+            //Move
+            Desition = 5;
+        }
+        if (Desition == 2)
+        {
+            //Brawl
+            Desition = 5;
+        }
+        if (Desition == 3)
+        {
+            //Reproduce
+            Desition = 5;
+        }
+        if (Desition == 4)
+        {
+            //Dont Move
+            Desition = 5;
+        }
+
+        Debug.Log(Desition);
+    
+      transform.rotation = Quaternion.Lerp(rotNow, rotSoon, 0.5f);
+      transform.position = Vector3.Lerp(posNow, posSoon, 0.5f);
+    }
+
+
+    void findFood ()
+    { 
+        GameObject []  gos;
         gos = GameObject.FindGameObjectsWithTag("Food");
         float distance = Mathf.Infinity;
-    Vector3 position = transform.position;
+        Vector3 position = transform.position;
+        
+
         foreach (GameObject go in gos)
         {
             Vector3 diff = go.transform.position - position;
-    float curDistance = diff.sqrMagnitude;
+            float curDistance = diff.sqrMagnitude;
             if (curDistance<distance)
             {
                 closest = go;
                 distance = curDistance;
-
             }
+
         }
+        float totx = closest.transform.position.x - spices.transform.position.x;
+        float toty = closest.transform.position.y - spices.transform.position.y;
+        rotSoon.Set(0, 0, Mathf.Atan2(toty, totx), 0);
+        rotNow = transform.rotation;
+        posSoon = closest.transform.position;
+        posNow = transform.position;
     }
 
-    void FixedUpdate () {
 
+    void OnTriggerEnter(Collider other)
+    {
 
+        Destroy(other);
+        Desition = 5;
+        foodFound = false;
 
-        startEatTime = startEatTime - Time.deltaTime;
-        
-
-
-
-        Quaternion rotation = Quaternion.LookRotation(closest.transform.position - transform.position, transform.TransformDirection(Vector3.up));
-        Quaternion soon = new Quaternion(0, 0, rotation.z, rotation.w);
-        Quaternion now = this.transform.rotation;
-        transform.rotation = Quaternion.Lerp(now, soon, 0.02f);
-        transform.position = Vector3.Lerp(transform.position, closest.transform.position, 0.1f);
-        closest = null;
     }
+
 }
